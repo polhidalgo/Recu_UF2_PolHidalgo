551775098cb835f045bc6959f3322db4c16313e3
polhidalgo <polhidalgolle7@gmail.com>
2025-06-02 16:04:58 +0200

    exercici6

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from fastapi import FastAPI
-from typing import List
+from fastapi import FastAPI, HTTPException
 from services.user import (
-    create_user
+    delete_user
 )
 
 app = FastAPI()
 
 
-@app.post("/users", response_model=dict)
-async def create_user_endpoint(
-    name: str, email: str, surname: str, description: str, course: str, year: int, street: str, postal_code: int, password: str
-):
-    new_user = create_user(name, email, surname, description, course, year, street, postal_code, password)
-    return new_user
+
+@app.delete("/users/{user_id}", response_model=dict)
+async def delete_user_endpoint(user_id: int):
+    """
+    DELETE /users/{user_id}
+    Esborra un usuari.
+    """
+    success = delete_user(user_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Usuari no trobat")
+    return {"deleted": True}
+
 
 
 
--- services/user.py
+++ services/user.py
@@ -1,17 +1,19 @@
 import config.database
-from schemas.user_sch import users_schema, user_schema
 
 
-def create_user(name: str, email: str, surname: str, description: str, course: str, year: int, street: str, postal_code: int, password: str) -> dict:
+def delete_user(user_id: int) -> bool:
     conn = config.database.connection_db()
     cursor = conn.cursor()
     try:
-        sql = "INSERT INTO users (name, surname, email, description, course, year, street, postal_code, password) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id, name, email"
-        cursor.execute(sql, (name, surname, email, description, course, year, street, postal_code, password))
-        new_row = cursor.fetchone()
-        conn.commit()
-        return user_schema(new_row)
+        sql = "DELETE FROM users WHERE id = %s RETURNING id"
+        cursor.execute(sql, (user_id,))
+        deleted = cursor.fetchone()
+        if deleted:
+            conn.commit()
+            return True
+        else:
+            conn.rollback()
+            return False
     finally:
         cursor.close()
         conn.close()
-
