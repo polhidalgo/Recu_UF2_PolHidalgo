78830f68351a09579e02191f7e79477f4117d7e0
polhidalgo <polhidalgolle7@gmail.com>
2025-06-02 16:06:12 +0200

    main

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from fastapi import FastAPI
-from typing import List
-from services.user import (
-    create_user
-)
 
-app = FastAPI()
-
-
-@app.post("/users", response_model=dict)
-async def create_user_endpoint(
-    name: str, email: str, surname: str, description: str, course: str, year: int, street: str, postal_code: int, password: str
-):
-    new_user = create_user(name, email, surname, description, course, year, street, postal_code, password)
-    return new_user
 
 
 
