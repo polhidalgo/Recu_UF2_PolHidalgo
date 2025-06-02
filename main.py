d4872711918a393465614206642b1400d9237e85
polhidalgo <polhidalgolle7@gmail.com>
2025-06-02 16:01:37 +0200

    commit

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ from services.user import (
 app = FastAPI()
 
 
-@app.post("/users", response_model=dict)
-async def create_user_endpoint(
-    name: str, email: str, surname: str, description: str, course: str, year: int, street: str, postal_code: int, password: str
-):
-    new_user = create_user(name, email, surname, description, course, year, street, postal_code, password)
-    return new_user
-
 @app.get("/users/{user_id}", response_model=dict)
 async def read_user(user_id: int):
     user = get_user_by_id(user_id)
--- services/user.py
+++ services/user.py
@@ -2,20 +2,6 @@ import config.database
 from schemas.user_sch import users_schema, user_schema
 
 
-def create_user(name: str, email: str, surname: str, description: str, course: str, year: int, street: str, postal_code: int, password: str) -> dict:
-    conn = config.database.connection_db()
-    cursor = conn.cursor()
-    try:
-        sql = "INSERT INTO users (name, surname, email, description, course, year, street, postal_code, password) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id, name, email"
-        cursor.execute(sql, (name, surname, email, description, course, year, street, postal_code, password))
-        new_row = cursor.fetchone()
-        conn.commit()
-        return user_schema(new_row)
-    finally:
-        cursor.close()
-        conn.close()
-
-
 def get_user_by_id(user_id: int) -> dict | None:
     conn = config.database.connection_db()
     cursor = conn.cursor()
